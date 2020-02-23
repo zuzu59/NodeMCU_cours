fe0e06e6339dfa1e6e242b6ba4f46b9470d002ab
Christian Zufferey <christian@zufferey.com>
2020-02-23 19:39:20 +0100

    Voilà, cela à l'air de bien fonctionner, faut tester encore sur une machine vierge ;-)

--- install.sh
+++ install.sh
@@ -1,27 +1,27 @@
 #!/usr/bin/env bash
 #Install de tout ce qu'il faut pour bien donner un cours de NodeMCU ;-)
-#zf200223.1928
+#zf200223.1934
 
-echo ---------- install des utilitaires à zuzu
+echo "---------- install des utilitaires à zuzu"
 
 cd
 git clone https://github.com/zuzu59/deploy-proxmox.git
 ./deploy-proxmox/install_utils.sh 
 
-echo ---------- install des utilitaires pour bien fonctionner ;-)
+echo "---------- install des utilitaires pour bien fonctionner"
 sudo apt install -y screen
 
-echo ---------- install ESPlorer
+echo "---------- install ESPlorer"
 cd NodeMCU_cours/
 unzip ESPlorer.zip
 
-echo ---------- autorisation dialout
+echo "---------- autorisation dialout"
 sudo usermod -a -G dialout xubuntu
 
 
 
 
-echo ---------- ATTENTION, il faut se déconecter/reconnecter pour que l'on puisse utiliser le port série !
+echo "---------- ATTENTION, il faut se déconecter/reconnecter pour que l'on puisse utiliser le port série !"
 
 
 
--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #Petit script pour bien démarrer l'IDE ESPlorer ;-)
-#zf200223.1918
+#zf200223.1936
 
-java -jar ~/NodeMCU_cours/ESPlorer/ESPlorer.jar
+java -jar ~/NodeMCU_cours/ESPlorer/ESPlorer.jar&
 
