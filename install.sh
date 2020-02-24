d7a2228ab6112a59ebefce7d2b7205cea387511d
Christian Zufferey <christian@zufferey.com>
2020-02-24 17:30:21 +0100

    Ajouté encore l'installation de esptool avec le pip install qui me manquait :-(

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #Install de tout ce qu'il faut pour bien donner un cours de NodeMCU ;-)
-#zf200224.1653
+#zf200224.1728
 
 echo "---------- install des utilitaires à zuzu"
 
@@ -10,14 +10,19 @@ git clone https://github.com/zuzu59/deploy-proxmox.git
 ./deploy-proxmox/install_utils.sh 
 
 echo "---------- install des utilitaires pour bien fonctionner"
-sudo apt install -y screen default-jre
+sudo apt install -y screen default-jre python-pip chromium-browser
 
 echo "---------- install ESPlorer"
-cd NodeMCU_cours/
+cd NodeMCU_Lua
+mkdir Tools
+cd Tools
+cp ~/NodeMCU_cours/ESPlorer.zip ./
 unzip ESPlorer.zip
 
 echo "---------- install esptool"
 git clone https://github.com/espressif/esptool.git
+mv esptool esptool-master
+pip install esptool
 
 echo "---------- autorisation dialout"
 sudo usermod -a -G dialout xubuntu
--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 #Petit script pour bien démarrer l'IDE ESPlorer ;-)
-#zf200223.1936
+#zf200224.1714
 
-java -jar ~/NodeMCU_cours/ESPlorer/ESPlorer.jar&
+#java -jar ~/NodeMCU_cours/ESPlorer/ESPlorer.jar&
+cd ~/NodeMCU_Lua
+./start_ESPlorer.sh
 
